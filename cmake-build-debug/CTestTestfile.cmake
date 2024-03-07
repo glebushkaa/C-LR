76edab0e7d703ef779e276d38830fd4fce2414b9
gle.bushkaa <gleb.mokryy@gmail.com>
2024-03-07 11:57:19 +0200

    folders changes

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hlib/CLionProjects/lr-1
-# Build directory: /Users/hlib/CLionProjects/lr-1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestBufferOverflow]=] "/Users/hlib/CLionProjects/lr-1/cmake-build-debug/lr_1_test" "0")
-set_tests_properties([=[TestBufferOverflow]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/hlib/CLionProjects/lr-1/CMakeLists.txt;9;add_test;/Users/hlib/CLionProjects/lr-1/CMakeLists.txt;0;")
--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hlib/CLionProjects/lr-1/tests
-# Build directory: /Users/hlib/CLionProjects/lr-1/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestBufferOverflow]=] "/Users/hlib/CLionProjects/lr-1/cmake-build-debug/tests/lr_1_test" "0")
-set_tests_properties([=[TestBufferOverflow]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/hlib/CLionProjects/lr-1/tests/CMakeLists.txt;3;add_test;/Users/hlib/CLionProjects/lr-1/tests/CMakeLists.txt;0;")
